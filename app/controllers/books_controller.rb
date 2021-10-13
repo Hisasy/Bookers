d67b88817425a6ebe0de7bcfc58a0bed6bb8b380
Hisasy7 <ac4317hisa@gmail.com>
2021-10-13 13:49:31 +0000

    2021.10.13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,46 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+
   end
 
   def show
-  end
-
-  def new
-    @book = Book.new
+    @book = Book.find(params[:id])
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
-    @book.save
-    redairect_to books_path
+    if @book.save
+      flash[:success] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
+  # def new
+  # end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
   private
+  # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
   end
